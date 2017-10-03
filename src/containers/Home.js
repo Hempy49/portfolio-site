93964796812b06b119dd09dae62a42dff7b5dbbb
Simon Hempenstall <simonjhemp@hotmail.com>
2017-10-03 22:57:13 +0100

    Add routes.js

---
+++ src/containers/Home.js
@@ -0,0 +1,12 @@
+import React, {Component, PropTypes} from 'react';
+
+export default class Home extends Component {
+  static propTypes = {};
+  render() {
+    return (
+      <div>
+        <h1>Hi home</h1>
+      </div>
+    );
+  }
+}
