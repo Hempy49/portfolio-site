1ac7512b1513f477be7f493a1932bc8f3784349c
Simon Hempenstall <simonjhemp@hotmail.com>
2017-10-04 11:47:15 +0100

    Add image, text and styling to home page

---
+++ src/theme/globalStyle.js
@@ -0,0 +1,16 @@
+import { injectGlobal } from 'styled-components';
+
+injectGlobal`
+@import url('https://fonts.googleapis.com/css?family=Aldrich');
+
+body {
+  font-family: 'Aldrich', sans-serif;
+  font-size: 25px;
+}
+h1 {
+  text-align: center;
+}
+h2 {
+  text-align: center;
+}
+`;
